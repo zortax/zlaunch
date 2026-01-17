cdc935f6454d0958f76b82b6e644be408e2927a9
Derock <derock@derock.dev>
2026-01-17 17:42:17 -0500

    ref: use cargoHash and patchelf

--- flake.nix
+++ flake.nix
@@ -19,7 +19,6 @@
           
           nativeBuildInputs = with pkgs; [
             pkg-config
-            makeWrapper
           ];
 
           buildInputs = with pkgs; [
@@ -31,26 +30,11 @@
             fontconfig
           ];
 
-          cargoDeps = pkgs.rustPlatform.importCargoLock {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              "collections-0.1.0" = "sha256-zh+9n9h3Vu7BbFczueXs3dC5sObMEPbKJMIS9YQPqYc=";
-              "xim-ctext-0.3.0" = "sha256-pRT4Sz1JU9ros47/7pmIW9kosWOGMOItcnNd+VrvnpE=";
-              "zbus-5.11.0" = "sha256-MM6KukXwiU6tovU+mJODjBhJ/OiHRdC6Yp1qPG9XlR4=";
-              "zed-font-kit-0.14.1-zed" = "sha256-rxpumYP0QpHW+4e+J1qo5lEZXfBk1LaL/Y0APkUp9cg=";
-              "zed-reqwest-0.12.15-zed" = "sha256-p4SiUrOrbTlk/3bBrzN/mq/t+1Gzy2ot4nso6w6S+F8=";
-              "zed-scap-0.0.8-zed" = "sha256-BihiQHlal/eRsktyf0GI3aSWsUCW7WcICMsC2Xvb7kw=";
-            };
-          };
+          cargoHash = "sha256-4MIGbfsXbsCdz3f6axO5AwoeH4oLpWxy4UI8qzYUZpY=";
 
-          postFixup = ''
-            wrapProgram $out/bin/zlaunch \
-              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [
-                pkgs.wayland
-                pkgs.libxkbcommon
-                pkgs.vulkan-loader
-                pkgs.xorg.libxcb
-              ]}
+          postFixup = with pkgs; ''
+            patchelf --add-rpath ${vulkan-loader}/lib $out/bin/zlaunch
+            patchelf --add-rpath ${wayland}/lib $out/bin/zlaunch
           '';
         };
 
